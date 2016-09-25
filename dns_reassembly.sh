1ec7c59973f50e8452a232788a431859640f9be3
David Bohannon <dbohannon@cigital.com>
2016-09-25 14:01:21 -0400

    Updated dns_reassembly.sh script

--- dns_reassembly.sh
+++ dns_reassembly.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Reassemble data exfiltrated via dns_export.sh
 #Example usage ./dns_reassembly.sh /var/log/syslog UNIQ_ID
-grep -e "[0-9a-f].*.$2" $1 | cut -d. -f1-2 | sort -t. -k2 -g -u | cut -d. -f1 | tr -d '\n' | xxd -r -p
+grep -o -e "[0-9a-f].*.$2" $1 | cut -d. -f1-2 | sort -t. -k2 -g -u | cut -d. -f1 | tr -d '\n' | xxd -r -p
